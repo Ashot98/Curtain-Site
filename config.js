ede3a7465264e0ad948cb4a544543f50f6487d48
Ashot <ashott98@gmail.com>
2018-01-12 23:18:37 +0400

    Mnac adminken

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  api_server: `http://localhost:8080/api/`,
+  api_server: `https://jakkard.herokuapp.com/api/`,
   mongo_server: `mongodb://ashota2:077371760a@ds133127.mlab.com:33127/jakkard`,
   image_path: `img/`
 }
