cf6b0bb32f84298796f773c944301de8ba32bebe
Ashot <ashott98@gmail.com>
2018-01-29 19:22:00 +0400

    asd

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  api_server: `188.225.74.179/api/`,
+  api_server: `http://188.225.74.179/api/`,
   mongo_server: `mongodb://ashota2:077371760a@ds133127.mlab.com:33127/jakkard`,
   image_path: `img/`
 }
