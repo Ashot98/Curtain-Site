86301a0988b5a482f67d9134ec6cde44625161d2
Ashot <ashott98@gmail.com>
2018-01-30 21:22:42 +0400

    salonjakkard.ru txa jan

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  api_server: `http://188.225.74.179:8080/api/`,
+  api_server: `http://salonjakkard.ru/api/`,
   mongo_server: `mongodb://ashota2:077371760a@ds133127.mlab.com:33127/jakkard`,
   image_path: `img/`
 }
